5d00748f85b57bb8709c9906bec919dfb0581d15
HossamCSE <166309300+HossamCSE@users.noreply.github.com>
2025-04-29 06:31:01 +0300

    Add Millionaire Game in C

---
+++ Func.h
@@ -0,0 +1,28 @@
+#ifndef FUNC_H_INCLUDED
+#define FUNC_H_INCLUDED
+
+#include<stdio.h>
+#include<conio.h>
+#include <stdlib.h>
+
+void Greeting(void);
+void Keep_or_Continue(void);
+void Wrong_ans(void);
+void Invalid_input(void);
+void Correct_ans(void);
+void choice_a(void);
+void choice_b(void);
+void choice_c(void);
+void choice_d(void);
+void Money_Won(void);
+void Congratulations(void);
+
+char q;
+char r;
+extern int money;
+extern int ques_num;
+extern int Counter;
+const char *Fifty;
+
+
+#endif
